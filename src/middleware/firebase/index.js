6badef54bfe383c7b70ee5d1f6ea1a384435b018
ofekziv <ofekdev21@gmail.com>
2021-10-20 14:35:33 +0300

    FSj21 scheduler branch test destinations

--- src/middleware/firebase/index.js
+++ src/middleware/firebase/index.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat';
 const app = require("firebase/app")
-cxvvcxcxvcxvcxvcv
+
 firebase.initializeApp( {
   apiKey: "AIzaSyARqN0HgQTBN4rM8SZWSlZXaVSb-u9BjeQ",
   authDomain: "schedulerflashdev.firebaseapp.com",
