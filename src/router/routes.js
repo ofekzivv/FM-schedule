34a6a71f169463d53e96f8449c7ccced13468c49
yuval Shalom <yuvalysh0@gmail.com>
2021-10-20 14:41:37 +0300

    Added the Calendars - day, week and Month. Also designed the mainLayout

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'DayView', component: () => import('pages/CalendarByDay.vue') },
+      { path: 'WeekView', component: () => import('pages/CalendarByWeek.vue') },
+      { path: 'MonthView', component: () => import('pages/CalendarByMonth.vue') }
     ]
   },
 
