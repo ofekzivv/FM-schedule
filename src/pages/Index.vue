2fccf0bd4ea3854a8e4939bc21171074d162a60f
yuval Shalom <yuvalysh0@gmail.com>
2021-10-21 15:41:02 +0300

    שיניתי קצת באגים

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,36 +1,33 @@
 <template>
   <q-page class="text-center my-font">
     <h5>שלום, לירן אלמליח</h5>
-    <q-card style="max-width: 900px; margin: 0 auto" dark>
+    <q-card style="max-width: 900px; margin: 0 auto">
       <q-tabs
         v-model="tab"
         dense
-        class="text-grey"
         active-color="primary"
         indicator-color="primary"
         align="justify"
         narrow-indicator
       >
-        <q-tab name="mails" label="משימות להיום" />
-        <q-tab name="addNewUser" label="הוספת משתמש חדש" />
-        <q-tab name="movies" label="מחיקת משתמש" />
+        <q-tab name="todayEvents" label="משימות להיום"/>
+        <q-tab name="addNewUser" label="הוספת משתמש חדש"/>
+        <q-tab name="deleteUser" label="מחיקת משתמש"/>
       </q-tabs>
 
-      <q-separator />
+      <q-separator/>
 
       <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="mails">
-          <div class="text-h6">Mails</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <q-tab-panel name="todayEvents">
+          <eventsCards/>
         </q-tab-panel>
 
         <q-tab-panel name="addNewUser">
           <AddNewUser/>
         </q-tab-panel>
 
-        <q-tab-panel name="movies">
-          <div class="text-h6">Movies</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <q-tab-panel name="deleteUser">
+          <DeleteUser/>
         </q-tab-panel>
       </q-tab-panels>
     </q-card>
@@ -39,16 +36,20 @@
 
 <script>
 import AddNewUser from "components/AddNewUser";
+import DeleteUser from "components/DeleteUser";
 import EventsCards from "components/eventsCards";
 import EventAdder from "components/EventAdder";
+import eventsCards from "components/eventsCards";
+
 export default {
   name: 'PageIndex',
   components: {
-    EventAdder, EventsCards, AddNewUser
+    DeleteUser,
+    EventAdder, EventsCards, AddNewUser, eventsCards
   },
-  data () {
+  data() {
     return {
-      tab: 'mails'
+      tab: 'todayEvents'
     }
   }
 }
