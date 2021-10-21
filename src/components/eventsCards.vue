e0b1d5bde96105fc54220807fc89b4f81f633393
yuval Shalom <yuvalysh0@gmail.com>
2021-10-21 12:13:35 +0300

    לא יודעת בדיוק מה שיניתי אבל תהנו

---
+++ src/components/eventsCards.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="cards">
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">פוסט</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">פוסט לראש השנה</div>
+          <div class="text-caption text-grey">
+            לכתוב פוסט לכבוד ראש השנה שכלול פה מלא דברים ועכשיו אני סתם חרטטת מילים אבל זה לא משנה כי זה יהיה הפוסט
+          </div>
+        </q-card-section>
+
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat>
+          18:30
+        </q-btn>
+        <q-btn flat color="primary">
+          25/10/2021
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">סרטון</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">סרטון ליום כיפור</div>
+          <div class="text-caption text-grey">
+            להעלות סרטון על כמה זה חשוב לצום ביום כיפור ושוב אני מחרטטת פה דברים כדי שיהיה עומק
+          </div>
+        </q-card-section>
+
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat>
+          15:30
+        </q-btn>
+        <q-btn flat color="primary">
+          28/10/2021
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "eventsCards"
+}
+</script>
+
+<style scoped>
+
+.my-card{
+  width: 100%;
+  max-width: 350px;
+}
+
+.cards {
+  display: flex;
+  flex-direction: column;
+  gap: 3em;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,52 +2,25 @@
   <q-page class="text-center my-font">
     <h4>שלום, יובל שלום</h4>
     <p>המשימות שלך להיום הן:</p>
-    <q-card class="my-card absolute-center" flat bordered>
-      <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-overline">Overline</div>
-          <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
-          <div class="text-caption text-grey">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
-        </q-card-section>
-
-        <q-card-section class="col-5 flex flex-center">
-          <q-img
-            class="rounded-borders"
-            src="https://cdn.quasar.dev/img/parallax2.jpg"
-          />
-        </q-card-section>
-      </q-card-section>
-
-      <q-separator />
-
-      <q-card-actions>
-        <q-btn flat round icon="event" />
-        <q-btn flat>
-          7:30PM
-        </q-btn>
-        <q-btn flat color="primary">
-          Reserve
-        </q-btn>
-      </q-card-actions>
-    </q-card>
+    <div>
+      <EventsCards/>
+    </div>
   </q-page>
 </template>
 
 <script>
+import EventsCards from "components/eventsCards";
 import EventAdder from "components/EventAdder";
 export default {
   name: 'PageIndex',
   components: {
-    EventAdder
+    EventAdder, EventsCards
   }
 }
 </script>
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 350px
+<style lang="scss" scoped>
+
+
 </style>
 
