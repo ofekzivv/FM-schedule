58283abb312a17cfa40be28006f4ebcc7e95314b
yuval Shalom <yuvalysh0@gmail.com>
2021-10-21 14:19:32 +0300

    Added add new user from admin panel

---
+++ src/components/AddNewUser.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <p>הוסף משתמש חדש:</p>
+    <form @submit.prevent="submitForm()">
+      <div class="row q-mb-md">
+        <q-input
+          outlined
+          v-model="formData.companyName"
+          class="col"
+          label="שם החברה"
+          lazy-rules
+          :rules="[val => val.length >=2 || 'Please enter at least 2 characters.']"
+        />
+      </div>
+      <div class="row q-mb-md">
+        <q-input
+          outlined
+          v-model="formData.email"
+          ref="email"
+          class="col"
+          label="מייל"
+          lazy-rules
+          :rules="[val => isValidEmailAddress(val) || 'Please enter a valid email address.']"
+        />
+      </div>
+      <div class="row q-mb-md">
+        <q-space/>
+        <q-btn
+          :disable="!formData.email || !formData.companyName"
+          label="הוסף משתמש"
+          type="submit"
+          color="primary"/>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import {addUser} from "src/middleware/firebase/database";
+
+export default {
+  name: "AddNewUser",
+  data() {
+    return {
+      formData: {
+        email: '',
+        companyName: ''
+      }
+    }
+  },
+  methods: {
+    isValidEmailAddress(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
+    submitForm() {
+      this.$refs.email.validate()
+      console.log('Submit')
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,55 @@
 <template>
   <q-page class="text-center my-font">
-    <h4>שלום, יובל שלום</h4>
-    <p>המשימות שלך להיום הן:</p>
-    <div>
-      <EventsCards/>
-    </div>
+    <h5>שלום, לירן אלמליח</h5>
+    <q-card>
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="mails" label="משימות להיום" />
+        <q-tab name="addNewUser" label="הוספת משתמש חדש" />
+        <q-tab name="movies" label="מחיקת משתמש" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="mails">
+          <div class="text-h6">Mails</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="addNewUser">
+          <AddNewUser/>
+        </q-tab-panel>
+
+        <q-tab-panel name="movies">
+          <div class="text-h6">Movies</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
+import AddNewUser from "components/AddNewUser";
 import EventsCards from "components/eventsCards";
 import EventAdder from "components/EventAdder";
 export default {
   name: 'PageIndex',
   components: {
-    EventAdder, EventsCards
+    EventAdder, EventsCards, AddNewUser
+  },
+  data () {
+    return {
+      tab: 'mails'
+    }
   }
 }
 </script>
