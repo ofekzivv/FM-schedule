4021b53f56d3be73cd4d8dba98741fd178b6d099
yuval Shalom <yuvalysh0@gmail.com>
2021-10-21 14:06:25 +0300

    תיקון יומן חודשי - לפי בחירת משתמש ספציפי והוספת משתמש

--- src/pages/CalendarByMonth.vue
+++ src/pages/CalendarByMonth.vue
@@ -1,5 +1,18 @@
 <template>
-  <MonthCalendar/>
+  <div class="my-font">
+    <div class="q-ma-lg">
+      <q-btn color="primary" label="הוסף משתמש" />
+    </div>
+    <div class="q-mt-lg items-center">
+      <div class="select text-center" style="width: 500px; margin: 2em auto;">
+        <p class="text-h5">בחר משתמש:</p>
+        <q-select v-model="model" :options="options" label="משתמש" />
+      </div>
+    </div>
+    <div>
+    <MonthCalendar v-if="model" :user="user"/>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +21,14 @@ import MonthCalendar from "components/MonthCalendar";
 export default {
   components: {
     MonthCalendar
+  },
+  data() {
+    return {
+      model: null,
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ]
+    }
   }
 }
 </script>
