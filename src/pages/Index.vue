516dc60a1d983f4fdc96ff03d1037acc8a417e2a
yuval Shalom <yuvalysh0@gmail.com>
2021-10-21 15:01:54 +0300

    Removed the register User

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="text-center my-font">
     <h5>שלום, לירן אלמליח</h5>
-    <q-card style="max-width: 900px; margin: 0 auto">
+    <q-card style="max-width: 900px; margin: 0 auto" dark>
       <q-tabs
         v-model="tab"
         dense
