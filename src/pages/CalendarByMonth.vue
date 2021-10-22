321145f43835666bf827c1c493515fe6c0c116a2
Yuval Shalom <yuvalysh0@gmail.com>
2021-10-22 15:00:09 +0300

    The calendar is changing every time we change company name. :)

--- src/pages/CalendarByMonth.vue
+++ src/pages/CalendarByMonth.vue
@@ -3,8 +3,7 @@
     <div class="q-mt-lg items-center">
       <div class="select text-center" style="max-width: 500px; width: 80%; margin: 2em auto;">
         <p class="text-h5">בחר משתמש:</p>
-        <q-select v-model="companyName" :options="options" label="משתמש" :disable="companyName" />
-        <q-btn v-if="companyName" color="dark" label="החלף משתמש" class="q-ma-sm" push @click="onClickDay2"/>
+        <q-select v-model="companyName" :options="options" label="משתמש" />
       </div>
     </div>
     <div>
@@ -14,7 +13,7 @@
 </template>
 
 <script>
-import {mapState, mapMutations, mapActions} from "vuex";
+import {mapState, mapActions} from "vuex";
 import MonthCalendar from "components/MonthCalendar";
 
 export default {
@@ -36,7 +35,11 @@ export default {
     for (let i = 0; i < this.users.length; i++) {
       this.options.push(this.users[i].companyName)
     }
-  }
+  },
+  /*watch: {
+    'companyName': function(val) {
+    }
+  }*/
 }
 </script>
 
