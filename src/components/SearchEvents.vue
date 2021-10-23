3a44fce0d354689891de2102e2f30163ea22a861
sharonlevi94 <sharonlevy96@gmail.com>
2021-10-23 18:38:37 +0300

    fsj21 sharon - added filtered events function in getters and SearchResults.vue page

--- src/components/SearchEvents.vue
+++ src/components/SearchEvents.vue
@@ -13,11 +13,11 @@
         <q-checkbox  v-model="searchKeys.type.image" label="תמונה" />
         <q-checkbox  v-model="searchKeys.type.video" label="סרטון" />
 
-        <q-input rounded filled v-model="searchKeys.startDate" label="סנן לפי תאריך">
+        <q-input rounded filled v-model="searchKeys.date" label="סנן לפי תאריך">
           <template v-slot:prepend>
             <q-icon name="event" class="cursor-pointer">
               <q-popup-proxy transition-show="scale" transition-hide="scale">
-                <q-date v-model="searchKeys.startDate" mask="YYYY-MM-DD">
+                <q-date v-model="searchKeys.date" mask="YYYY-MM-DD">
                   <div class="row items-center justify-end">
                     <q-btn v-close-popup label="Close" color="primary" flat/>
                   </div>
