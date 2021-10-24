738e1c7eee4a0c631661fccca956e277fcf50e82
gilad luria <giladluria@gmail.com>
2021-10-24 11:50:35 +0300

    functions update (create user)

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.setUserDataToFB = functions.auth.user().onCreate(userRecord => {
+  let newUser = {
+    fullName: userRecord.displayName,
+    email: userRecord.email,
+  }
+  admin.firestore().collection('usersInfo').doc(userRecord.uid).set(newUser)
+    .then(()=>{
+      console.log('newUser: ',newUser)
+    })
+})
