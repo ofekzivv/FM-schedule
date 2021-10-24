bd10ca0dadabe6eda5a2f632f4075783085408de
yuval Shalom <yuvalysh0@gmail.com>
2021-10-24 14:24:37 +0300

    changed firebase

--- src/middleware/firebase/index.js
+++ src/middleware/firebase/index.js
@@ -4,6 +4,7 @@ const app = require("firebase/app")
 firebase.initializeApp( {
   apiKey: "AIzaSyDVMVLFSZqzGq2bvBMM3Me8pKmycGs_Pbo",
   authDomain: "flashmediascheduler.firebaseapp.com",
+  databaseURL: "https://flashmediascheduler-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "flashmediascheduler",
   storageBucket: "flashmediascheduler.appspot.com",
   messagingSenderId: "876047916440",
